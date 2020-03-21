63e20fdc2995bc9f82f8dfa71482ed7fca34142b
bbfra <62484068+bbfra@users.noreply.github.com>
2020-03-21 21:42:51 +0100

    file aggiunti in upload

---
+++ Arduino/arduino Gaia/led_lampeggiante/led_lampeggiante.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(4, INPUT);
+}
+void loop() {
+if (digitalRead(4)) {
+  // do Thing A
+  digitalWrite(2, HIGH);
+}
+else {
+  // do Thing C
+  digitalWrite(2, LOW);
+} }
---
+++ Arduino/led_verde_gaia/led_verde_gaia.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(8,OUTPUT); // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(8, HIGH);
+  delay (500);
+  digitalWrite(8,LOW);
+  delay(500);// put your main code here, to run repeatedly:
+
+}
